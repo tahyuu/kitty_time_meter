911d880b83d7d416fba4c3fc32593f4d9fb61622
tahyuu <tahyuu@hotmail.com>
2018-09-04 16:09:13 +0800

    update the features

--- ketty.py
+++ ketty.py
@@ -73,7 +73,7 @@ class Example(QtGui.QWidget):
                                           background-color : NavajoWhite;
                                           height : 30px;
                                           border-style: outset;
-                                          font : 13px  ''')
+                                          font : 30px  ''')
 
         updateBtn.clicked.connect(lambda:self.taskStartEnd(id))
 
@@ -117,19 +117,23 @@ class Example(QtGui.QWidget):
         self.label_msg.setText(QtGui.QApplication.translate("MainWindow","208-09-03 11:30 Monday\n Welcome home, Child",None,QtGui.QApplication.UnicodeUTF8))
     def TimerStart(self):
         self.lcdNumber = QtGui.QLCDNumber(self)
-        self.lcdNumber.setGeometry(QtCore.QRect(10, 600, 1250, 280))
+        self.lcdNumber.setGeometry(QtCore.QRect(10, 500, 1250, 280))
         font = QtGui.QFont()
         font.setPointSize(13)
         self.lcdNumber.setFont(font)
         self.lcdNumber.setNumDigits(8)
         self.lcdNumber.setMode(QtGui.QLCDNumber.Bin)
         self.lcdNumber.setObjectName(_fromUtf8("lcdNumber"))
+        self.lcdNumber.setStyleSheet('''text-align : center;
+                                    background-color : LightCoral;
+                                    height : 30px;
+                                    border-style: outset;
+                                    font : 13px; ''')
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.updateDisplay)
     def taskStartEnd(self,i):
         if self.sender().text()=="Start" or self.sender().text()=="Continue":
             self.startTime=time.time()
-            print self.sender().text()
             if self.sender().text()=="Start":
                 self.task_list[int(i)][2]=time.strftime("%H:%M:%S", time.localtime())
             self.sender().setText("Finished")
@@ -137,7 +141,7 @@ class Example(QtGui.QWidget):
         else:
             self.sender().setText("Continue")
             self.task_list[int(i)][3]=time.strftime("%H:%M:%S", time.localtime())
-	    self.startTime=time.mktime(time.strptime(time.strftime("%Y/%m/%d ",time.localtime())+self.task_list[int(i)][2],"%Y/%m/%d %H:%M:%S"))
+            self.startTime=time.mktime(time.strptime(time.strftime("%Y/%m/%d ",time.localtime())+self.task_list[int(i)][2],"%Y/%m/%d %H:%M:%S"))
             timecost=time.time()-self.startTime
             self.timer.stop()
             hours=int(timecost/3600)
@@ -165,7 +169,7 @@ class Example(QtGui.QWidget):
         self.timer.timeout.connect(self.updateDisplay)
     def taskListInit(self):
         self.widget_task_list= QtGui.QTableWidget(self)
-        self.widget_task_list.setGeometry(QtCore.QRect(10,10,1260, 540))
+        self.widget_task_list.setGeometry(QtCore.QRect(10,10,1250, 490))
         self.widget_task_list.setObjectName(_fromUtf8("#table"))
         self.widget_task_list.clear()
         self.widget_task_list.setRowCount(10)
@@ -176,6 +180,22 @@ class Example(QtGui.QWidget):
         self.widget_task_list.setEditTriggers(QtGui.QTableWidget.NoEditTriggers)
         self.widget_task_list.setSelectionBehavior(QtGui.QTableWidget.SelectRows)
         self.widget_task_list.setSelectionMode(QtGui.QTableWidget.SingleSelection)
+        self.widget_task_list.setStyleSheet(''' text-align : center;
+                                          height : 30px;
+                                          border-style: outset;
+                                          font : 30px  ''')
+        header = self.widget_task_list.horizontalHeader()
+        header.setResizeMode(0, QtGui.QHeaderView.ResizeToContents)
+        header.setResizeMode(1, QtGui.QHeaderView.Stretch)
+        header.setResizeMode(2, QtGui.QHeaderView.ResizeToContents)
+        header.setResizeMode(3, QtGui.QHeaderView.ResizeToContents)
+        header.setResizeMode(4, QtGui.QHeaderView.ResizeToContents)
+        header.setResizeMode(5, QtGui.QHeaderView.ResizeToContents)
+        for i in range(10):
+            self.widget_task_list.setRowHeight(i,50)
+        #self.widget_task_list.setRowHeight(0,40)
+        stylesheet = "::section{Background-color:#A640BF;border-radius:4px;}"
+        self.widget_task_list.horizontalHeader().setStyleSheet(stylesheet)
         self.updateTaskList()
     def updateTaskList(self):
         rowid=self.currentTaskId
@@ -184,6 +204,7 @@ class Example(QtGui.QWidget):
         for row_number, row_data in enumerate(self.task_list):
             #self.widget_task_list.insertRow(row_number)
             #if row is -1 then update all the list
+            #self.widget_task_list.setRowHeight(row_number,50)
             if rowid==-1 or rowid==row_number:
                 pass
             else:

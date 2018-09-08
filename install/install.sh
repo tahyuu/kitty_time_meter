1462e7c3e5aa97f820c4c4e8952013b47f086bee
yong.tan <tahyuu@hotmail.com>
2018-09-08 00:14:08 +0000

    change install/install.sh

--- install/install.sh
+++ install/install.sh
@@ -2,4 +2,4 @@
 sudo apt-get update
 sudo apt-get install python-qt4
 sudo pip install ConfigParser
-echo "/home/pi/time_meter/clientStart.sh start">> /home/pi/.config/lxsession/LXDE-pi/autostart
+echo "/home/pi/time_meter/install/clientStart.sh start">> /home/pi/.config/lxsession/LXDE-pi/autostart

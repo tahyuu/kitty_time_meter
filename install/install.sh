94c77a3f6f730a284281815241dea715d81a8df4
tahyuu <tahyuu@hotmail.com>
2018-11-12 20:33:07 +0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -2,4 +2,5 @@
 sudo apt-get update
 sudo apt-get install python-qt4
 sudo pip install ConfigParser
+sudo pip install ps4
 echo "/home/pi/time_meter/install/clientStart.sh start">> /home/pi/.config/lxsession/LXDE-pi/autostart
